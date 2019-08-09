cbcfffe4292b6b5ffd53b41e8ebae8fb16318a3c
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-09 18:13:06 +0100

    included routes for the services

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var router = express.Router()
-const authorize = require('../controllers/authorizeUser')
-
+const calenderEvents = require('../controllers/calenderEvents')
+const authorizeUser = require('../controllers/authorizeUser')
+const authenticateUser = require('../controllers/authenticateUser')
 /* GET home page. */
 router
     .get('/', function(req, res, next) {
         res.render('index', { title: 'Express' })
     })
-    .post('/', authorize)
+    .post('/authorize-user', authorizeUser)
+    .get('/calender-events', calenderEvents)
+    .post('/authenticate-user', authenticateUser)
 module.exports = router
